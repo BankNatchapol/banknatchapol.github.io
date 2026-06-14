7c6540ba30926d33b0bff46a06b4b5df3c7de7c1
BankNatchapol <BankNatchapol@users.noreply.github.com>
2026-06-14 15:01:51 +0000

    deploy: 4c3586d57c96c24cd9fc5e49ba7428b0187ec7d2

--- assets/channel-BQC8JoLl.js
+++
@@ -1,1 +0,0 @@
-import{U as a,D as n}from"./index-TE0I6MRc.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/channel-IEp6o8yV.js
@@ -0,0 +1,1 @@
+import{U as a,D as n}from"./index-D4T9VCPX.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
